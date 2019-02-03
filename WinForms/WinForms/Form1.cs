c9a2a1602ae1b4ae25a5024372a1a22e9706fbf2
Joseph <joseph.cunningham@wsu.edu>
2019-02-02 22:41:02 -0800

    Wrote unit testmethods for each of the three methods in the form1 class

--- WinForms/WinForms/Form1.cs
+++ WinForms/WinForms/Form1.cs
@@ -22,8 +22,69 @@ namespace WinForms
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
+            //initialize the list of integers 
+            List<int> integers = new List<int>();
+            //initialize the random number generator
+            Random rand = new Random();
+
+            //load the list with random integers
+            integers = LoadList(integers, rand);
+
+            //string Convert = integers[0].ToString();
+            //this.textBox1.Text = Convert;
+        }
+
+        /// <summary>
+        /// Loads the input list with 10,000 random integers in the range [0, 20,000]
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="rand"></param>
+        /// <returns name="input"></returns>
+        public List<int> LoadList(List<int> input, Random rand)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                input.Add(rand.Next(20000));
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public String Task1()
+        {
+            
+            return "";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public String Task2()
+        {
+           
+            return "";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public String Task3()
+        {
+           
+            return "";
         }
     }
 }
--- WinForms/WinFormsUnitTest/UnitTest1.cs
+++ WinForms/WinFormsUnitTest/UnitTest1.cs
@@ -1,14 +1,61 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinForms;
+using System.Collections.Generic;
 
 namespace WinFormsUnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class WinFormsTests
     {
+        /// <summary>
+        /// 
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void Task1Test()
+        {  
+            //initialize the form object and other variables for testing
+            Form1 f = new Form1();
+            List<int> integers = new List<int>();
+            Random rand = new Random();
+
+            //load the list and check if loaded
+            integers = f.LoadList(integers, rand);
+            Assert.IsNotNull(integers);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void Task2Test()
         {
+            //initialize the form object and other variables for testing
+            Form1 f = new Form1();
+            List<int> integers = new List<int>();
+            Random rand = new Random();
+
+            //load the list and check if loaded
+            integers = f.LoadList(integers, rand);
+            Assert.IsNotNull(integers);
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void Task3Test()
+        {
+            //initialize the form object and other variables for testing
+            Form1 f = new Form1();
+            List<int> integers = new List<int>();
+            Random rand = new Random();
+
+            //load the list and check if loaded
+            integers = f.LoadList(integers, rand);
+            Assert.IsNotNull(integers);
         }
     }
 }

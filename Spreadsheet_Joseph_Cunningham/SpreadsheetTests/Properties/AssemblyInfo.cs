c59e33fefbb5a6d766a18ad73ac1b8244f31661e
Joseph <joseph.cunningham@wsu.edu>
2019-02-16 19:58:01 -0800

    implemented the interface of the rows and columns and unit tests

---
+++ Spreadsheet_Joseph_Cunningham/SpreadsheetTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpreadsheetTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpreadsheetTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e50c4b88-8598-46a9-80b5-828016bf79b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Spreadsheet_Joseph_Cunningham/Spreadsheet_Joseph_Cunningham/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spreadsheet_Joseph_Cunningham
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+
+            // create columns A-Z
+            dataGridView1.ColumnCount = 26;
+            for (int i = 0; i < 26; i++)
+            {
+                char asci = (char)(65 + i);
+                dataGridView1.Columns[i].HeaderText = asci.ToString();
+            }
+
+            //create 50 rows
+            dataGridView1.RowCount = 50;
+            dataGridView1.RowHeadersVisible = true;
+            for (int i = 0; i < 50; i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            }
+        }
+    }
+}

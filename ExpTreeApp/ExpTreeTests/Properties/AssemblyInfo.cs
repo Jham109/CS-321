ad26c31611f725ee1a1bd95977e03afe511a0989
Joseph <joseph.cunningham@wsu.edu>
2019-02-28 14:31:50 -0800

    initialized Unit test project, Logic Engine DLL, and Console application

---
+++ ExpTreeApp/ExpTreeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExpTreeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExpTreeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ffcc3af7-8ca5-4270-b716-c6938bc86635")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExpTreeApp/LogicEngine/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cpts321
+{
+    public class ExpressionTree
+    {
+        public ExpressionTree(string expression)
+        {
+
+        }
+
+        void SetVariable(string variableName, double variableValue)
+        {
+
+        }        double Evaluate()
+        {
+            return 0;
+        }
+    }
+}
